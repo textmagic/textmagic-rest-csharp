0dac8e7ee80bf662a889adc32b1abb0509955647
foufure <jonathan_orditz@hotmail.com>
2015-09-23 21:41:09 +0200

    [ADD] added Ping implementation in Ping.cs and Ping tests in PingTest.cs.

---
+++ TextmagicRest/Ping.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RestSharp;
+using RestSharp.Authenticators;
+using RestSharp.Deserializers;
+using TextmagicRest.Model;
+using RestSharp.Validation;
+
+namespace TextmagicRest
+{
+    public partial class Client
+    {
+        /// <summary>
+        /// Ping the API
+        /// </summary>
+        /// <returns></returns>
+        public PingResult Ping()
+        {
+            var request = new RestRequest();
+            request.Resource = "ping";
+
+            return Execute<PingResult>(request);
+        }
+    }
+}
