3e74bf58691a00e8ca2806489625f49ac94694f5
foufure <jonathan_orditz@hotmail.com>
2015-09-23 21:23:21 +0200

    [ADD] added working example project.

---
+++ TextmagicRest.Examples/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using TextmagicRest;
+using TextmagicRest.Model;
+
+namespace textmagicsample
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            sendMessage();
+
+            Console.WriteLine("Check your phone ;-)");
+            Console.WriteLine("Press any key to continue . . . ");
+            Console.ReadKey(true);
+        }
+
+        public static void sendMessage()
+        {
+            var client = new Client("replace-with-username", "replace-with-user-api-key");
+            var link = client.SendMessage("Hello from TextMagic API C# Wrapper demo application", "replace-with-phoneNumber-WithInternationalPrefix");
+
+            if (link.Success)
+            {
+                Console.WriteLine("Message session {0} successfully sent", link.Id);
+            }
+            else
+            {
+                Console.WriteLine("Message was not sent due to following exception: " + link.ClientException.Message);
+            }
+        }
+    }
+}
